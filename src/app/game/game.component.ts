781662b6010a19e50058ce48e6958132588d0bdf
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-08-06 13:13:39 +0200

    Mitspielerliste designen

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,44 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Game } from '../../models/game'; 
-
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component'; 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
   pickCardAnimation = false;
   currentCard: string = '';
-  game!: Game;  //! Definite assignment assertion dass die Variable nicht undefined oder null ist
-
-constructor() { }
-
-ngOnInit(): void {
-  this.newGame();
-}
-
-newGame() {
-  this.game = new Game();
-  console.log('New game started:', this.game);
-}
-
-takeCard() {
-  if(!this.pickCardAnimation){  // nur wenn pickCardAnimation false ist, kann eine Karte genommen werden
-  this.currentCard = this.game.stack.pop() ?? ''; // ?? '' bedeuten dass, wenn der Wert undefined ist, ein leerer String verwendet wird
- 
-  this.pickCardAnimation = true;
- 
-   console.log('new Card:'+ this.currentCard);
-  console.log('Game is:' ,this.game);
-
-
-  setTimeout(() => { 
-    this.game.playedCards.push(this.currentCard);
-    this.pickCardAnimation = false;
-  }, 1000); 
-}
-}
+  game!: Game; //! Definite assignment assertion dass die Variable nicht undefined oder null ist
+  constructor() {}
+  ngOnInit(): void {
+    this.newGame();
+  }
+  newGame() {
+    this.game = new Game();
+    console.log('New game started:', this.game);
+  }
+  takeCard() {
+    if (!this.pickCardAnimation) {// nur wenn pickCardAnimation false ist, kann eine Karte genommen werden
+      this.currentCard = this.game.stack.pop() ?? ''; // ?? '' bedeuten dass, wenn der Wert undefined ist,
+      // ein leerer String verwendet wird
+      this.pickCardAnimation = true;
+      console.log('new Card:' + this.currentCard);
+      console.log('Game is:', this.game);
+           setTimeout(() => {
+              this.game.playedCards.push(this.currentCard);
+              this.pickCardAnimation = false;
+             }, 1000);
+    }
+  }
 }
